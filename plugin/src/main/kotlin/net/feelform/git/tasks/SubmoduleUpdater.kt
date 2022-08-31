fb005dc9ff2eca8ac9c510483404fea74409b6ac
Yongseok Kang <feelform@gmail.com>
2022-08-31 21:04:08 +0900

    [#2] output file

--- plugin/src/main/kotlin/net/feelform/git/tasks/SubmoduleUpdater.kt
+++ plugin/src/main/kotlin/net/feelform/git/tasks/SubmoduleUpdater.kt
@@ -1,9 +1,16 @@
 package net.feelform.git.tasks
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+
 
 abstract class SubmoduleUpdater : DefaultTask() {
     @get:Input
@@ -12,10 +19,24 @@ abstract class SubmoduleUpdater : DefaultTask() {
     @get:Input
     abstract val relativePath: Property<String>
 
+    @get:OutputFile
+    abstract val destination: RegularFileProperty
+
     @TaskAction
     fun update() {
         val url = this.url.get()
         val path = this.relativePath.get()
         logger.quiet("Successfully resolved URL '$url' path '$path'")
+    }
+
+    @Throws(IOException::class)
+    private fun writeFile(destination: File, content: String) {
+        var output: BufferedWriter? = null
+        try {
+            output = BufferedWriter(FileWriter(destination))
+            output.write(content)
+        } finally {
+            output?.close()
+        }
     }
 }
